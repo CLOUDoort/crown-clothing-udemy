110156456cabd08a10c17fa19ce522d1b41c6fa3
JunSeok <ppu1234@naver.com>
2023-03-25 12:39:36 +0900

    Add navigation bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import './navigation.styles.scss'
+
+import { Link, Outlet } from "react-router-dom"
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import { Fragment } from "react"
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <CrownLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet /> 
+        </Fragment>
+    )
+}
+
+export default Navigation
